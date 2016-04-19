5c7961f5099d86a7651761762ff7d92e4a8bd53c
drewtuley <drewtuley@gmail.com>
2016-04-19 15:28:40 +0100

    import into PyCharm and attempt to comply with PEP8 (or whatever) and tidy the format

--- src/MontyHall/MontyHall.py
+++ src/MontyHall/MontyHall.py
@@ -4,44 +4,46 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+from random import choice, seed
 
 __author__="andrew.tuley"
 __date__ ="$09-Mar-2016 13:25:39$"
 
-from random import choice, seed;
 
-doors = [];
-win=1;
-lose=2;
+doors = []
+win = 1
+lose = 2
+
 
 def setup_doors():
     while doors.count(win) <1: 
-        doors.append(win);
+        doors.append(win)
     while doors.count(lose) <2: 
-        doors.append(lose);
-    
+        doors.append(lose)
+
+
 def play(tries, change):
     """ 
     Play the Monty Hall game <tries> times and either <change> or not when 
     given the choice.
     """
-    wins=0;
-    loop=0;
+    wins = 0
+    loop = 0
     while loop < tries:
-        setup_doors();
-        door = choice(doors);
+        setup_doors()
+        door = choice(doors)
 
-        doors.remove(lose);
+        doors.remove(lose)
         # you're now given the chance to change your choice
         if change:
-            door = choice(doors);
+            door = choice(doors)
         if door == win:
-            wins += 1;
-        loop += 1;    
-    print ('I won {0} times ({1}%) by {2}'.format(wins, (wins*100/tries), 'changing' if change else 'not changing' ));    
+            wins += 1
+        loop += 1    
+    print ('I won {0:,} times out of {3:,} ({1:.2%}) by {2}'.format(wins, (wins/tries), 'changing' if change else 'not changing', tries))
 
 if __name__ == "__main__":
-    seed();
+    seed()
     
-    play(100000,True);
-    play(100000,False);
+    play(1000000, True)
+    play(1000000, False)
