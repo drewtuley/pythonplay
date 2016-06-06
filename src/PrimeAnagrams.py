98386e3743f5ad402e24ce3d9da09bbb4e403aa6
drewtuley <drewtuley@gmail.com>
2016-06-06 15:56:02 +0100

    considerable speed improvement due to bisect_left and some tweaking. but still only finds 2 word anagrams

--- src/PrimeAnagrams.py
+++ src/PrimeAnagrams.py
@@ -1,5 +1,6 @@
 import math
 import string
+from bisect import bisect_left
 
 
 def is_prime(n):
@@ -11,7 +12,7 @@ def is_prime(n):
     return True
 
 
-primes = [p for p in range(11, 150) if is_prime(p)]
+primes = [p for p in range(3, 150) if is_prime(p)]
 
 
 def calc_pval(str):
@@ -50,7 +51,8 @@ with open('words.txt') as fd:
                         words.append(word)
                     anaprimes_rev[prime] = words
         except UnicodeDecodeError as err:
-            print('error {} '.format(err))
+            # print('error {} '.format(err))
+            pass
         else:
             complete = True
 print('Processed {} anaprimes'.format(len(anaprimes)))
@@ -59,13 +61,43 @@ print(anaprimes['enumerations'])
 print(anaprimes['mountaineers'])
 print(len(anaprimes_rev))
 print(anaprimes_rev[anaprimes['mountaineers']])
-for prime in anaprimes_rev:
-    if len(anaprimes_rev[prime]) > 1:
-        print('Prime:{} anagrams={}'.format(prime, anaprimes_rev[prime]))
+# for prime in anaprimes_rev:
+#     if len(anaprimes_rev[prime]) > 1:
+#         print('Prime:{} anagrams={}'.format(prime, anaprimes_rev[prime]))
 
-treasure = anaprimes['treasure']
-hunt = anaprimes['hunt']
-product = treasure * hunt
-l=[(w1,w2) for w1 in anaprimes_rev.keys() for w2 in anaprimes_rev.keys() if w1*w2 == product]
-for w1,w2 in l:
-    print('w1:{} w2:{}'.format(anaprimes_rev[w1], anaprimes_rev[w2]))
+anaprimes_rev.pop(-1)
+sorted_keys = list(anaprimes_rev.keys())
+sorted_keys.sort()
+
+source = ['treasure', 'hunt']
+product = 1
+for word in source:
+    product *= anaprimes[word]
+
+# for x in sorted_keys:
+#     if x>40000:
+#         break
+#     print('key={} words={}'.format(x, anaprimes_rev[x]))
+
+upper_idx = len(sorted_keys)
+for w1 in sorted_keys:
+    if w1 > product:
+        break
+    if w1 < 0:
+        next
+
+    w2_lower = product / w1
+    w2_idx = bisect_left(sorted_keys, w2_lower) - 1
+    if w2_idx >= 0 and w2_idx < upper_idx:
+        w2 = sorted_keys[w2_idx]
+        while (w1 * w2) <= product and w2 < product and w2 >= w1:
+            if w2 < w1:
+                next
+            if (w1 * w2) == product:
+                print('w1:{}[{}] w2:{}[{}]'.format(anaprimes_rev[w1], w1, anaprimes_rev[w2], w2))
+
+            w2_idx += 1
+            if w2_idx < upper_idx:
+                w2 = sorted_keys[w2_idx]
+            else:
+                break
