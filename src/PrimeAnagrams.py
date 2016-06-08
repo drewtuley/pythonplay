9c538cecfc3933b275f82ffca0be2442f923fb9a
drewtuley <drewtuley@gmail.com>
2016-06-08 08:22:43 +0100

    use recursive search method (not fully optimised yet)

--- src/PrimeAnagrams.py
+++ src/PrimeAnagrams.py
@@ -1,6 +1,7 @@
 import math
 import string
-from bisect import bisect_left
+import functools
+import operator
 
 
 def is_prime(n):
@@ -47,7 +48,7 @@ with open('words.txt') as fd:
                         words = anaprimes_rev[prime]
                         words.append(word)
                     else:
-                        words = []
+                        words = list()
                         words.append(word)
                     anaprimes_rev[prime] = words
         except UnicodeDecodeError as err:
@@ -57,47 +58,82 @@ with open('words.txt') as fd:
             complete = True
 print('Processed {} anaprimes'.format(len(anaprimes)))
 
-print(anaprimes['enumerations'])
-print(anaprimes['mountaineers'])
-print(len(anaprimes_rev))
-print(anaprimes_rev[anaprimes['mountaineers']])
+# print(anaprimes['enumerations'])
+# print(anaprimes['mountaineers'])
+# print(len(anaprimes_rev))
+# print(anaprimes_rev[anaprimes['mountaineers']])
 # for prime in anaprimes_rev:
 #     if len(anaprimes_rev[prime]) > 1:
 #         print('Prime:{} anagrams={}'.format(prime, anaprimes_rev[prime]))
 
-anaprimes_rev.pop(-1)
+if -1 in anaprimes_rev:
+    anaprimes_rev.pop(-1)
 sorted_keys = list(anaprimes_rev.keys())
 sorted_keys.sort()
 
-source = ['treasure', 'hunt']
+source = ['andrew', 'tuley']
 product = 1
 for word in source:
-    product *= anaprimes[word]
+    try:
+        pval = anaprimes[word]
+    except:
+        pval = calc_pval(word)
+    product *= pval
 
+print('product for {} is {}'.format(source, product))
 # for x in sorted_keys:
 #     if x>40000:
 #         break
 #     print('key={} words={}'.format(x, anaprimes_rev[x]))
 
-upper_idx = len(sorted_keys)
-for w1 in sorted_keys:
-    if w1 > product:
-        break
-    if w1 < 0:
-        next
+try:
+    anagrams = anaprimes_rev[product]
+    print('single word anagrams={}'.format(anagrams))
+except:
+    print('no single word anagrams')
 
-    w2_lower = product / w1
-    w2_idx = bisect_left(sorted_keys, w2_lower) - 1
-    if w2_idx >= 0 and w2_idx < upper_idx:
-        w2 = sorted_keys[w2_idx]
-        while (w1 * w2) <= product and w2 < product and w2 >= w1:
-            if w2 < w1:
-                next
-            if (w1 * w2) == product:
-                print('w1:{}[{}] w2:{}[{}]'.format(anaprimes_rev[w1], w1, anaprimes_rev[w2], w2))
 
-            w2_idx += 1
-            if w2_idx < upper_idx:
-                w2 = sorted_keys[w2_idx]
-            else:
-                break
+def inc_indexes(indices, index, index_count, max_index, product, sorted_keys):
+    if index == index_count - 1:
+        prod = 1
+        for x in indices[:-1]:
+            prod *= sorted_keys[x]
+
+        if product % prod == 0:
+            my_start_idx = int(product / prod)
+            if my_start_idx not in sorted_keys:
+                return
+            my_start = sorted_keys.index(my_start_idx)
+            if my_start <= indices[index - 1]:
+                return
+        else:
+            return
+    elif index - 1 >= 0:
+        my_start = indices[index - 1] + 1
+    else:
+        my_start = 0
+
+    if index < index_count - 1 and my_start >= max_index:
+        return
+
+    indices[index] = my_start
+    while indices[index] < max_index:
+        if index + 1 < index_count:
+            yield from (inc_indexes(indices, index + 1, index_count, max_index, product, sorted_keys))
+        else:
+            if indices[index_count - 2] != indices[index_count - 1]:
+                yield (indices)
+        if index == index_count - 1:
+            break
+        else:
+            indices[index] += 1
+
+
+print(len(anaprimes_rev))
+indices = [0 for x in range(3)]
+for idxs in inc_indexes(indices, 0, len(indices), len(anaprimes_rev), product, sorted_keys):
+    words = ''
+    for idx in idxs:
+        factor = sorted_keys[idx]
+        words = '{} w:{}[{}]'.format(words, anaprimes_rev[factor], factor)
+    print(words)
