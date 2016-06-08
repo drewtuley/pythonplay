2e4918f838ac82af3f45c23426dc27bb486abb2f
drewtuley <drewtuley@gmail.com>
2016-06-08 16:00:18 +0100

    sanitise the word list and MAJOR performance improvements in the finding algorithm

--- src/PrimeAnagrams.py
+++ src/PrimeAnagrams.py
@@ -1,6 +1,7 @@
 import math
 import string
-import functools
+from itertools import combinations
+from functools import reduce
 import operator
 
 
@@ -23,7 +24,7 @@ def calc_pval(str):
             idx = string.ascii_lowercase.index(l)
             pval = primes[idx]
         except Exception as ex:
-            # print('Trouble with: {} in {} because {}'.format(l, str, ex))
+            #print('Trouble with: {} in {} because {}'.format(l, str, ex))
             pval = 0
         if prod < 0:
             prod = pval
@@ -52,28 +53,21 @@ with open('words.txt') as fd:
                         words.append(word)
                     anaprimes_rev[prime] = words
         except UnicodeDecodeError as err:
-            # print('error {} '.format(err))
+            print('error {} '.format(err))
             pass
         else:
             complete = True
 print('Processed {} anaprimes'.format(len(anaprimes)))
 
-# print(anaprimes['enumerations'])
-# print(anaprimes['mountaineers'])
-# print(len(anaprimes_rev))
-# print(anaprimes_rev[anaprimes['mountaineers']])
-# for prime in anaprimes_rev:
-#     if len(anaprimes_rev[prime]) > 1:
-#         print('Prime:{} anagrams={}'.format(prime, anaprimes_rev[prime]))
 
 if -1 in anaprimes_rev:
     anaprimes_rev.pop(-1)
 sorted_keys = list(anaprimes_rev.keys())
 sorted_keys.sort()
 
-source = ['andrew', 'tuley']
+source = ['treasure']
 product = 1
-for word in source:
+for word in [s.strip().lower() for s in source]:
     try:
         pval = anaprimes[word]
     except:
@@ -81,10 +75,6 @@ for word in source:
     product *= pval
 
 print('product for {} is {}'.format(source, product))
-# for x in sorted_keys:
-#     if x>40000:
-#         break
-#     print('key={} words={}'.format(x, anaprimes_rev[x]))
 
 try:
     anagrams = anaprimes_rev[product]
@@ -93,47 +83,17 @@ except:
     print('no single word anagrams')
 
 
-def inc_indexes(indices, index, index_count, max_index, product, sorted_keys):
-    if index == index_count - 1:
-        prod = 1
-        for x in indices[:-1]:
-            prod *= sorted_keys[x]
+matching_prime_products = list()
+for k in sorted_keys:
+    if product % k == 0:
+        matching_prime_products.append(k)
 
-        if product % prod == 0:
-            my_start_idx = int(product / prod)
-            if my_start_idx not in sorted_keys:
-                return
-            my_start = sorted_keys.index(my_start_idx)
-            if my_start <= indices[index - 1]:
-                return
-        else:
-            return
-    elif index - 1 >= 0:
-        my_start = indices[index - 1] + 1
-    else:
-        my_start = 0
+print('Found {} matching prime products'.format(len(matching_prime_products)))
 
-    if index < index_count - 1 and my_start >= max_index:
-        return
+for r in combinations(matching_prime_products, 2):
+    if reduce(operator.mul, r, 1) == product:
+        words = ''
+        for factor in r:
+            words = '{} w:{}[{}]'.format(words, anaprimes_rev[factor], factor)
+        print(words)
 
-    indices[index] = my_start
-    while indices[index] < max_index:
-        if index + 1 < index_count:
-            yield from (inc_indexes(indices, index + 1, index_count, max_index, product, sorted_keys))
-        else:
-            if indices[index_count - 2] != indices[index_count - 1]:
-                yield (indices)
-        if index == index_count - 1:
-            break
-        else:
-            indices[index] += 1
-
-
-print(len(anaprimes_rev))
-indices = [0 for x in range(3)]
-for idxs in inc_indexes(indices, 0, len(indices), len(anaprimes_rev), product, sorted_keys):
-    words = ''
-    for idx in idxs:
-        factor = sorted_keys[idx]
-        words = '{} w:{}[{}]'.format(words, anaprimes_rev[factor], factor)
-    print(words)
